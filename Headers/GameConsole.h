682e41f39619c9f6992028a3fe12d08099830ca4
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2023-12-20 16:22:50 -0300

    [TNS-666] Implemented the GameConsole to center all the I/O

---
+++ Headers/GameConsole.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include <string_view>
+
+class GameConsole
+{
+public:
+    static void Print(std::string_view message);
+    static void PrintLine(std::string_view message);
+
+    template<typename T>
+    static T Read(std::string_view message)
+    {
+        T readData;
+        std::cout << message;
+        std::cin >> std::ws >> readData;
+
+        return readData;
+    }
+};
