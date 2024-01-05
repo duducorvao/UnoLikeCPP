0b517da82853f4eb89ba8ad00c51bbadb882d0f0
duducorvao <eduardobruno@hotmail.co.uk>
2024-01-05 17:57:40 -0300

    [TNS - 856] Fixed player hand size config

--- Headers/Config.h
+++ Headers/Config.h
@@ -5,7 +5,7 @@ class Config
 public:
     static constexpr int PLAYERS_MIN_AMOUNT {2};
     static constexpr int PLAYERS_MAX_AMOUNT {10};
-    static constexpr int PLAYERS_HAND_SIZE {2};
+    static constexpr int PLAYERS_HAND_SIZE {7};
     static constexpr int PLAYERS_HAND_SIZE_TO_SAY_UNO {2};
     static constexpr int YELL_UNO_PENALTY_CARDS {2};
 
